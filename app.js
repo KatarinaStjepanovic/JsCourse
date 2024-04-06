107f78a372c8beb193fd6203b8505de5ed90834c
KatarinaStjepanovic <165832565+KatarinaStjepanovic@users.noreply.github.com>
2024-04-06 21:30:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function zbir(num1,num2){
+  return num1+num2;
+}
+
+var zbir=zbir(1,2);
+console.log(zbir);
+
+function razlika(num1,num2){
+    return num1-num2;
+  }
+  
+  var razlika=razlika(1,2);
+  console.log(razlika);
+
+  function proizvod(num1,num2){
+    return num1*num2;
+  }
+  
+  var proizvod=proizvod(1,2);
+  console.log(proizvod);
+
+  function količnik(num1,num2){
+    return num1/num2;
+  }
+  
+  var količnik=količnik(1,2);
+  console.log(količnik);
+
+  function zbir(num1,num2){
+    return num1+num2;
+  }
+  
+  var zbir=zbir(1,2);
+  console.log(zbir);
