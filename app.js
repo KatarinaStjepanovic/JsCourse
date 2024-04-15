0ab5b704afc55cfd3c5a979cc5927332e0f5e1b4
KatarinaStjepanovic <165832565+KatarinaStjepanovic@users.noreply.github.com>
2024-04-15 15:13:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,100 @@
-function zbir(num1,num2){
+/*function zbir(num1,num2){
   return num1+num2;
 }
 
-var zbir=zbir(1,2);
+let zbir=zbir(1,2);
 console.log(zbir);
 
 function razlika(num1,num2){
     return num1-num2;
   }
   
-  var razlika=razlika(1,2);
+  let razlika=razlika(1,2);
   console.log(razlika);
 
   function proizvod(num1,num2){
     return num1*num2;
   }
   
-  var proizvod=proizvod(1,2);
+  let proizvod=proizvod(1,2);
   console.log(proizvod);
 
   function količnik(num1,num2){
     return num1/num2;
   }
   
-  var količnik=količnik(1,2);
+  let količnik=količnik(1,2);
   console.log(količnik);
 
   function zbir(num1,num2){
     return num1+num2;
   }
   
-  var zbir=zbir(1,2);
-  console.log(zbir);
+  let zbir=zbir(1,2);
+  console.log(zbir);*/
+
+
+
+
+
+  let CurrentResult=0;
+
+  addBtn.addEventListener("click",add);
+
+  function add(){
+    let CaloculationDescript=CurrentResult + "+" + userInput.value;
+    CurrentResult=CurrentResult+parseInt(userInput.value);
+     outputResult(CurrentResult,CaloculationDescript);
+
+
+  }
+
+
+  subtractBtn.addEventListener("click",substract);
+
+
+  function substract(){
+    let CalculationDescript=CurrentResult + "-" + userInput.value;
+    CurrentResult=CurrentResult-parseInt(userInput.value);
+    outputResult(CurrentResult,CalculationDescript);
+  }
+
+
+  multiplyBtn.addEventListener("click",multiply);
+
+
+
+  function multiply(){
+    let CalculationDescript=CurrentResult + "*" +userInput.value;
+    CurrentResult=CurrentResult*parseInt(userInput.value);
+    outputResult(CurrentResult,CalculationDescript);
+
+  }
+
+
+  divideBtn.addEventListener("click",divide);
+
+  function divide(){
+    let CalculationDescript=CurrentResult + "/" + userInput.value;
+    CurrentResult=CurrentResult/parseInt(userInput.value);
+    outputResult(CurrentResult,CalculationDescript);
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
