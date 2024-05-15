6f9374977404bb98f154ef6de821fe07806d4010
KatarinaStjepanovic <165832565+KatarinaStjepanovic@users.noreply.github.com>
2024-05-15 22:37:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 
   let CurrentResult=0;
 
+  let resultHistory=[0];
+
   addBtn.addEventListener("click",add);
 
+
+
+  function addHistoryLog(userInput,operation,operationResult){
+    let logEntry={
+      inp: userInput,
+      opr: operation,
+      res: operationResult
+
+    }
+    resultHistory.push(logEntry);
+    console.log(resultHistory);
+  }
+
+ 
   function add(){
     let CaloculationDescript=CurrentResult + "+" + userInput.value;
     CurrentResult=CurrentResult+parseInt(userInput.value);
      outputResult(CurrentResult,CaloculationDescript);
+ 
+    addHistoryLog(parseInt(userInput.value),"ADD",CurrentResult);
+    
 
 
   }
+ 
 
 
   subtractBtn.addEventListener("click",substract);
@@ -19,6 +39,8 @@
     let CalculationDescript=CurrentResult + "-" + userInput.value;
     CurrentResult=CurrentResult-parseInt(userInput.value);
     outputResult(CurrentResult,CalculationDescript);
+    addHistoryLog(parseInt(userInput.value),"SUB",CurrentResult);
+
   }
 
 
@@ -30,6 +52,8 @@
     let CalculationDescript=CurrentResult + "*" +userInput.value;
     CurrentResult=CurrentResult*parseInt(userInput.value);
     outputResult(CurrentResult,CalculationDescript);
+    addHistoryLog(parseInt(userInput.value),"MLT",CurrentResult);
+
 
   }
 
@@ -40,6 +64,8 @@
     let CalculationDescript=CurrentResult + "/" + userInput.value;
     CurrentResult=CurrentResult/parseInt(userInput.value);
     outputResult(CurrentResult,CalculationDescript);
+    addHistoryLog(parseInt(userInput.value),"DIV",CurrentResult);
+
 
 
   }
@@ -50,6 +76,8 @@
     let CalculationDescript= CurrentResult + " ˇ2";
    CurrentResult= CurrentResult * CurrentResult;
    outputResult(CurrentResult,CalculationDescript);
+   addHistoryLog(parseInt(userInput.value),"DBL",CurrentResult);
+
 
   }
 
@@ -60,6 +88,8 @@
     let CalculationDescript= CurrentResult + "^" + userInput.value;
     CurrentResult= Math.pow(CurrentResult,parseInt(userInput.value));
     outputResult(CurrentResult,CalculationDescript);
+    addHistoryLog(parseInt(userInput.value),"STP",CurrentResult);
+
  
 
   }
